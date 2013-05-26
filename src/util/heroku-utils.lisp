b07201e9e5d3ec135cc2db3f9f3dedeb157387ae
Dmitriy Rybalko <rdtft@me.com>
2013-05-26 13:48:19 +0300

    heroku-specific utils settings

--- src/util/heroku-utils.lisp
+++ src/util/heroku-utils.lisp
@@ -4,7 +4,7 @@
   #+ccl (ccl:getenv target)
   #+sbcl (sb-posix:getenv target))
 
-(defun db-params ()
+(defparameter db-params
   (let* ((url (second (cl-ppcre:split "//" (heroku-getenv "DATABASE_URL"))))
 	 (user (first (cl-ppcre:split ":" (first (cl-ppcre:split "@" url)))))
 	 (password (second (cl-ppcre:split ":" (first (cl-ppcre:split "@" url)))))
